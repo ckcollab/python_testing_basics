6047c6b17a04bfcec74fc6594734279ef7e83a3c
Eric Carmichael <eric@ckcollab.com>
2014-12-17 13:05:49 -0800

    explanation + better test

--- types of tests/integration/tests.py
+++ types of tests/integration/tests.py
@@ -10,10 +10,14 @@ def get_weather(city_name):
 class TestWeatherIntegration(unittest.TestCase):
     '''Many modules are tested together, any one point could fail or the API response
     could have changed since we last called it'''
-    def test_get_weather_returns_temp_min_and_max(self):
+    def test_get_weather_returns_temp(self):
         result = get_weather("Coeur d'Alene, Idaho")
         data = json.loads(result.content)
         assert "temp" in data["main"]
+
+        # How could we improve this test? All it's looking for is the "temp" property to exist. We could also make sure
+        # the returned value is of the right type!
+        assert float(data["main"]["temp"])
 
 
 if __name__ == "__main__":
