820a87f7b3accbb15b48033dbf1cc6f1866c034e
Eric Carmichael <eric@ckcollab.com>
2014-12-17 13:00:19 -0800

    fixes line spacing

--- types of tests/unit/tests.py
+++ types of tests/unit/tests.py
@@ -1,11 +1,14 @@
 import unittest
 
+
 def add_one(number):
     return number + 1
+
 
 class TestAddOne(unittest.TestCase):
     def test_add_one_returns_number_plus_one(self):
         assert add_one(1) == 2
+
 
 if __name__ == "__main__":
     unittest.main()
