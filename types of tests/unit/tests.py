77aca92b8f69bdc7f0f6e2c71259c13f97dcbdde
Eric Carmichael <eric@ckcollab.com>
2014-12-17 18:50:19 -0800

    failing test

--- types of tests/unit/tests.py
+++ types of tests/unit/tests.py
@@ -7,7 +7,7 @@ def add_one(number):
 
 class TestAddOne(unittest.TestCase):
     def test_add_one_returns_number_plus_one(self):
-        assert add_one(1) == 2
+        assert add_one(1) == 3
 
 
 if __name__ == "__main__":
